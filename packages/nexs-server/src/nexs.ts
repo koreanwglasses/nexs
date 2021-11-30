488bc2c498cc750fd64f10a19ae5524ede5864c0
koreanwglasses <koreanwglasses@gmail.com>
2021-11-30 11:10:51 -0600

    feat: improved stack initialization

--- packages/nexs-server/src/nexs.ts
+++ packages/nexs-server/src/nexs.ts
@@ -1,11 +1,11 @@
 import Next from "next";
 import Express from "express";
-import { Server as IO } from "socket.io";
+import { Server as IO, ServerOptions as IOServerOptions } from "socket.io";
 import type { Socket } from "socket.io";
 import http from "http";
-import type https from "https";
+import https from "https";
 
-import expressSession from "express-session";
+import ExpressSession from "express-session";
 import type { Session } from "express-session";
 
 import iosession from "express-socket.io-session";
@@ -30,34 +30,60 @@ interface IOWithSession extends IO {
 
 function nexs({
   next: next_,
+  nextOpts = {},
+
   session: session_,
+  sessionOpts = {},
+
   express: express_,
+
   io: io_,
+  ioOpts = {},
+
   server: server_,
+  serverOpts = {},
+
   dev,
 }: {
-  session?: Express.RequestHandler;
   next?: NextServer;
+  nextOpts?: Parameters<typeof Next>[0];
+
+  session?: Express.RequestHandler;
+  sessionOpts?: Partial<ExpressSession.SessionOptions>;
+
   express?: Express.Express;
+
   io?: IO;
+  ioOpts?: Partial<IOServerOptions>;
+
   server?: http.Server | https.Server;
+  serverOpts?: http.ServerOptions | https.ServerOptions;
+
   dev?: boolean;
 }) {
   // Next.js integration
-  const next = next_ ?? Next({ dev });
+  const next = next_ ?? Next({ ...nextOpts, dev });
   const handle = next.getRequestHandler();
 
-  // Initialize Express and Socket.IO
+  // Initialize Express and Socket.IO servers
   const express = express_ ?? Express();
-  const io = (io_ ?? new IO()) as IOWithSession;
+
+  const server =
+    server_ ??
+    ("key" in serverOpts && "cert" in serverOpts
+      ? https.createServer(serverOpts, express)
+      : http.createServer(serverOpts, express));
+
+  const io = (io_ ?? new IO(server, ioOpts)) as IOWithSession;
 
   // Session setup
   const session =
     session_ ??
-    expressSession({
+    ExpressSession({
       secret: "secret",
       resave: true,
       saveUninitialized: true,
+      ...sessionOpts,
     });
 
   express.use(session);
@@ -80,7 +106,7 @@ function nexs({
 
     socket.on("subscription:unsub", (dataKey) => {
       socket.leave(`subscribers:${dataKey}`);
-    })
+    });
 
     socket.on("disconnect", () => {
       const session = socket.handshake.session!;
@@ -89,19 +115,32 @@ function nexs({
     });
   });
 
-  // Forward all routes to Next.js
-  express.all("*", (req, res) => {
-    (req as any).io = io;
-    return handle(req, res);
-  });
+  const finalize = async () => {
+    await next.prepare();
 
-  const server = server_ ?? http.createServer(express);
+    // Forward all remaining routes to Next.js
+    express.all("*", (req, res) => {
+      (req as any).io = io;
+      return handle(req, res);
+    });
 
-  // Attach Socket.IO, which overrides the /socket.io* routes
-  io.attach(server);
+    // Attach Socket.IO, which overrides the /socket.io* routes
+    io.attach(server);
+  };
+
+  type ReturnPromise<T> = T extends (...args: infer A) => infer R
+    ? (...args: A) => Promise<R>
+    : never;
+  const listen: ReturnPromise<http.Server["listen"]> = async (...args) => {
+    await finalize();
+    return server.listen(...args);
+  };
 
   return {
+    finalize,
+    listen,
     next,
+    express,
     io,
     server,
   };
